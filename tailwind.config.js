27938eae63e439335a7761a4712078f0d7237b9b
hasanalam24 <hasanalam365@gmail.com>
2024-02-18 01:18:17 +0600

    best offers section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
-    },
-    plugins: [],
+        extend: {
+
+        },
+        plugins: [],
+    }
 }
