bfb8e1a953a077986a83009447016e18076ae3ac
hasanalam24 <hasanalam365@gmail.com>
2024-02-19 09:11:04 +0600

    modal done

--- script.js
+++ script.js
@@ -1,89 +1,95 @@
-// const cards = document.querySelectorAll(".cards");
+const cards = document.getElementsByClassName('cards');
 
-// let count = 0;
-// let allCount = 40;
-// // let totalCount = 1000;
-
-// for (const card of cards) {
-//     card.addEventListener('click', function () {
-//         card.style.backgroundColor = '#1DD100'
-//         card.style.color = 'white'
-
-//         // seat count
-//         const seatCount = document.getElementById('seatCount');
-//         count++
-//         seatCount.innerText = count;
+let count = 0;
+let available = 40;
 
 
-//         // Available seat
+for (const card of cards) {
 
-//         const availableseat = document.getElementById('available-seats');
-//         allCount--
-//         availableseat.innerText = allCount;
+    card.addEventListener('click', function () {
 
 
-//         // total price
-//         const totalPrice = document.getElementById('total-price');
-//         totalPrice.innerText = count * 550;
+        // console.log(title)
 
-//         // totalCount = totalPrice.innerText + totalCount
-//         // console.log(totalPrices)
+        const ticket = card.querySelector('span') /*.innerText;*/
 
-//         if (count >= 1) {
-//             card.setAttribute("disabled", true);
-//         }
+        card.style.backgroundColor = '#1DD100'
+        card.style.color = 'white'
 
-
-//     })
-// }
+        const seatCount = document.getElementById('seatCount');
+        // const convertSeatType = parseFloat(seatCount);
+        count++
+        seatCount.innerText = count;
 
 
 
+        const availableSeats = document.getElementById('available-seats');
+        // console.log(availableSeats)
+        // const available = parseFloat(availableSeats)
+        // console.log(available)
+        available--
+        availableSeats.innerText = available;
 
-// // const btnApply = document.getElementById('apply-btn');
-
-// // btnApply.addEventListener('click', function () {
-// //     const inputField = document.getElementById('input-field').value;
-// //     // console.log(inputField)
-
-// //     const discountPrice = document.getElementById('grand-total-price');
-
-// //     if (inputField === 'SELL200') {
-
-// //         discountPrice.innerText = count * 550 * 20 / 100;
-// //     }
-// //     else {
-// //         alert('Invalid Coupon Code')
-// //     }
-
-// //     // total before discount
-
-// //     const afterDiscountPrice = document.getElementById('total')
-// //     afterDiscountPrice.innerText = sumTotal - discountPrice.innerText;
-
-// // })
+        // console.log(availableSeats)
 
 
-// // document.getElementById('input-field').addEventListener('keyup', function (event) {
-// //     const inputField = event.target.value;
-// //     const applyBtn = document.getElementById('apply-btn')
-// //     if (inputField === "NEW15" || inputField === "Couple 20") {
-// //         applyBtn.removeAttribute('disabled');
-// //     }
+        const totalPrice = document.getElementById('total-price');
 
-// //     // else {
-// //     //     applyBtn.setAttribute('disabled', true)
-// //     // }
-// //     document.getElementById('apply-btn').addEventListener('click', function () {
-// //         const grandTotal = document.getElementById('grand-total-price');
-
-// //         const discount = total2 * 15 / 100;
-// //         grandTotal.innerText = total2 - discount;
-// //     })
-// // })
+        // const convertPrice = parseFloat(totalPrice)
+        const Price = count * 550;
+        totalPrice.innerText = Price;
+        console.log(Price)
+        if (count >= 1) {
+            card.setAttribute("disabled", true);
+        }
+        const grandTotal = document.getElementById('grand-total');
+        grandTotal.innerText = Price
 
 
+    })
+}
 
-// // const availableseat = document.getElementById('available-seats');
-// // allCount--
-// // availableseat.innerText = allCount;
+
+const applyBtn = document.getElementById('apply-btn').addEventListener('click', function () {
+
+    const Price = count * 550;
+    const inputField = document.getElementById('input-field').value;
+    if (inputField === 'NEW15') {
+        const discount = Price * 15 / 100;
+        const afterDiscountPrice = Price - discount;
+        const grandTotal = document.getElementById('grand-total');
+        grandTotal.innerText = afterDiscountPrice;
+    }
+    if (inputField === 'Couple 20') {
+        const discount = Price * 20 / 100;
+        const afterDiscountPrice = Price - discount;
+        const grandTotal = document.getElementById('grand-total');
+        grandTotal.innerText = afterDiscountPrice;
+    }
+})
+
+document.getElementById('input-field').addEventListener('keyup', function (event) {
+    const inputField = event.target.value;
+    const applyBtn = document.getElementById('apply-btn')
+    if (inputField === "NEW15" || inputField === "Couple 20") {
+        applyBtn.removeAttribute('disabled');
+    }
+
+    else {
+        applyBtn.setAttribute('disabled', true)
+    }
+})
+
+
+// next btn
+
+const nextBtn = document.getElementById('next-btn').addEventListener('click', function () {
+
+    showElement('success')
+
+})
+
+function showElement(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('hidden')
+}
